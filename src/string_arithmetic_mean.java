ac18c77aaca421dadc041b5a032a60b732d00635
JasonSaber <jsaber19@windwardschool.org>
2018-08-28 12:50:03 -0700

    initial

---
+++ src/string_arithmetic_mean.java
@@ -0,0 +1,19 @@
+
+public class string_arithmetic_mean {
+    public static void main(String[] args){
+        System.out.println(findMean("41 18 -20 4 0 18"));
+    }
+    public static double findMean(String s){
+        double denom = 0;
+        int numerator = 0;
+        String[] nums = s.split(" ");
+        for (String i : nums){
+            numerator+=Integer.parseInt(i);
+        }
+        for (int i = 0; i<s.length(); i++){
+            if (s.charAt(i)==' ') denom++;
+        }
+        denom++;
+        return numerator/denom;
+    }
+}
